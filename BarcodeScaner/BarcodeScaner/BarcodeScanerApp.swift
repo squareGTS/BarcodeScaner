d7fb8d635d85401651a71c128b4bdbb90eca58a2
Maxim <68609996+squareGTS@users.noreply.github.com>
2022-11-25 15:49:18 +0300

    Add files via upload

---
+++ BarcodeScaner/BarcodeScaner/BarcodeScanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BarcodeScanerApp.swift
+//  BarcodeScaner
+//
+//  Created by Maxim Bekmetov on 25.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct BarcodeScanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
